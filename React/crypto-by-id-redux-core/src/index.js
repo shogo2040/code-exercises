1c099a9905e185a01ab25bb3aba4492818555b86
Rey Arqueza <reyarqueza@gmail.com>
2022-08-19 11:50:33 -0700

    redux core controlled component

--- React/crypto-by-id-redux-core/src/index.js
+++ React/crypto-by-id-redux-core/src/index.js
@@ -1,4 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>start</div>, document.getElementById("root"));
+// redux core dependencies
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+// connect redux to react component.
+import { connect } from "react-redux";
+
+const setCryptoIdInput = (cryptoId) => {
+  return {
+    type: "CRYPTO_ID_INPUT",
+    cryptoId,
+  };
+};
+
+const reducer = (state = { cryptoId: "" }, action) => {
+  switch (action.type) {
+    case "CRYPTO_ID_INPUT":
+      return {
+        ...state,
+        cryptoId: action.cryptoId,
+      };
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+function handleCryptoIdInput(cryptoIdInput, setCryptoIdInput) {
+  setCryptoIdInput(cryptoIdInput);
+}
+
+function mapStateToProps(state) {
+  return {
+    cryptoId: state.cryptoId,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setCryptoIdInput: (cryptoId) => {
+      dispatch(setCryptoIdInput(cryptoId));
+    },
+  };
+}
+
+function App({ cryptoId, setCryptoIdInput }) {
+  return (
+    <>
+      <h1>Crypto by id</h1>
+      <p>{cryptoId}</p>
+      <input
+        type="text"
+        onChange={(e) => {
+          handleCryptoIdInput(e.target.value, setCryptoIdInput);
+        }}
+        value={cryptoId}
+      />
+    </>
+  );
+}
+
+const AppConnected = connect(mapStateToProps, mapDispatchToProps)(App);
+ReactDOM.render(
+  <Provider store={store}>
+    <AppConnected />
+  </Provider>,
+  document.getElementById("root")
+);
