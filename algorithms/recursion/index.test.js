7100b4860c8da314d7bf65649b1120f2868002d5
Rey Arqueza <reyarqueza@gmail.com>
2022-08-30 18:28:16 -0700

    recursive count, recursive max num

--- algorithms/recursion/index.test.js
+++ algorithms/recursion/index.test.js
@@ -1,4 +1,9 @@
-import { sum, euclidsAlgorithm } from "./index";
+import {
+  sum,
+  euclidsAlgorithm,
+  recursiveCount,
+  recursiveMaxNum,
+} from "./index";
 
 describe("Sum of array of numbers using recursion", () => {
   const numbers = [1, 2, 5, 15, 22, 23, 99];
@@ -17,7 +22,39 @@ describe("Sum of array of numbers using recursion", () => {
 });
 
 describe("Euclid's algorithm - Greatest common divisor", () => {
-  test("Split this land up in the largest equal parts", () => {
+  test("1 of 1 - Split this land up in the largest equal parts", () => {
     expect(euclidsAlgorithm(1680, 640)).toBe(80);
+  });
+});
+
+describe("write a recursive function to count the number of items in a list", () => {
+  const list = ["carrots", "lettuce", "onions", "beef", "potatos"];
+
+  test("1 of 3 - nonzero list", () => {
+    expect(recursiveCount(list)).toBe(5);
+  });
+
+  test("2 of 3 - 1 element list", () => {
+    expect(recursiveCount(["lala"])).toBe(1);
+  });
+
+  test("3 of 3 - empty list", () => {
+    expect(recursiveCount([])).toBe(0);
+  });
+});
+
+describe("write a recursive function to find the maximum number in the list", () => {
+  let numbers = [5, 6, 1, 82, 2, 55, 21, 8];
+
+  test("1 of 3 - nonzero list", () => {
+    expect(recursiveMaxNum(numbers)).toBe(82);
+  });
+
+  test("2 of 3 - 1 element list", () => {
+    expect(recursiveMaxNum([9])).toBe(9);
+  });
+
+  test("3 of 3 - empty list", () => {
+    expect(recursiveMaxNum([])).toBe(0);
   });
 });
