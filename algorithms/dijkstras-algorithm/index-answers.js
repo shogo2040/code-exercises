dbdc980e1a5b3663fde9319847f333ccb2fdb956
Rey Arqueza <reyarqueza@gmail.com>
2022-09-10 14:35:46 -0700

    dijkstras algorithm and unit tests

--- algorithms/dijkstras-algorithm/index-answers.js
+++ algorithms/dijkstras-algorithm/index-answers.js
@@ -1,38 +1,74 @@
 // Graph (an abstract data structure)
-// Breadth first search
+// dijkstrasAlgorithm
 
-export function breadthFirstSearch(me, graph) {
-  const friends = graph.get(me).friends;
-  let foundFriend;
+function initHashTables(graph, startKey, endKey) {
+  const startHashTable = graph.get(startKey);
+  const costs = new Map();
+  const parents = new Map();
 
-  while (friends && friends.length > 0) {
-    for (const friend of friends) {
-      const friendObj = graph.get(friend);
+  startHashTable.forEach((value, key) => {
+    costs.set(key, value);
+    parents.set(key, startKey);
+  });
 
-      if (!friendObj) {
-        continue;
-      }
+  costs.set(endKey, Infinity);
+  parents.set(endKey, "");
 
-      if (friendObj.isMangoSeller) {
-        foundFriend = friendObj;
-        break;
-      } else {
-        const theirFriends = friendObj.friends;
-        for (const theirFriend of theirFriends) {
-          if (friends.includes(theirFriend)) {
-            continue;
-          }
-          friends.push(theirFriend);
-        }
-      }
-    }
+  return {
+    costs,
+    parents,
+  };
+}
 
-    if (foundFriend && foundFriend.isMangoSeller) {
-      break;
-    }
+function sortMapByValue(map) {
+  // sorts in ascending order / smallest to largest
+  return new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
+}
 
-    friends.shift();
+function getPath(startKey, key, parents) {
+  if (key === startKey) {
+    return [startKey];
+  } else {
+    return [key, ...getPath(startKey, parents.get(key), parents)];
   }
+}
 
-  return foundFriend;
+export function dijkstrasAlgorithm(graph, startKey, endKey) {
+  const { costs, parents } = initHashTables(graph, startKey, endKey);
+  graph.forEach((value, currentKey) => {
+    const neighbors = graph.get(currentKey);
+
+    if (!neighbors) {
+      return;
+    }
+
+    // sort the neighbors and name it currentNeighbors
+    // because the shortest path ir processed first
+    const currentNeighbors = sortMapByValue(neighbors);
+
+    currentNeighbors.forEach((cnValue, cnKey) => {
+      const neighborNeighbors = graph.get(cnKey);
+
+      if (!neighborNeighbors) {
+        return;
+      }
+
+      neighborNeighbors.forEach((nnValue, nnKey) => {
+        const costOfNNKey = costs.get(nnKey);
+
+        if (!costOfNNKey) {
+          costs.set(nnKey, nnValue + costs.get(cnKey));
+          parents.set(nnKey, cnKey);
+        } else if (costOfNNKey > nnValue + costs.get(cnKey)) {
+          costs.set(nnKey, nnValue + costs.get(cnKey));
+          parents.set(nnKey, cnKey);
+        }
+      });
+    });
+  });
+
+  return {
+    cost: costs.get(endKey),
+    path: getPath(startKey, endKey, parents).reverse(),
+  };
 }
--- algorithms/dijkstras-algorithm/index.test.js
+++ algorithms/dijkstras-algorithm/index.test.js
@@ -1,118 +1,90 @@
-import { breadthFirstSearch } from "./index";
+import { dijkstrasAlgorithm } from "./index";
 
-describe("breadth first search - graphs", () => {
-  const graph = new Map();
+describe("dijkstras algorithm - weighted graphs", () => {
+  test("graph of book to piano test", () => {
+    const graph = new Map();
+    const book = new Map();
+    const lp = new Map();
+    const poster = new Map();
+    const guitar = new Map();
+    const drums = new Map();
 
-  graph.set("A", {
-    isMangoSeller: false,
-    friends: ["B"],
-  });
+    book.set("LP", 5);
+    book.set("Poster", 0);
+    lp.set("Guitar", 15);
+    lp.set("Drums", 20);
+    poster.set("Guitar", 30);
+    poster.set("Drums", 35);
+    guitar.set("Piano", 20);
+    drums.set("Piano", 10);
 
-  graph.set("B", {
-    isMangoSeller: false,
-    friends: ["X", "Z"],
-  });
+    graph.set("Book", book);
+    graph.set("LP", lp);
+    graph.set("Poster", poster);
+    graph.set("Guitar", guitar);
+    graph.set("Drums", drums);
+    graph.set("Piano", null);
 
-  graph.set("me", {
-    isMangoSeller: false,
-    friends: ["B", "D"],
-  });
-
-  graph.set("D", {
-    isMangoSeller: false,
-    friends: ["W", "E", "U"],
-  });
-
-  graph.set("E", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  graph.set("U", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  graph.set("W", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  graph.set("X", {
-    isMangoSeller: false,
-    friends: ["Y"],
-  });
-
-  graph.set("Y", {
-    isMangoSeller: true,
-    friends: [],
-  });
-
-  graph.set("Z", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  const graphNoManguSeller = new Map();
-
-  graphNoManguSeller.set("A", {
-    isMangoSeller: false,
-    friends: ["B"],
-  });
-
-  graphNoManguSeller.set("B", {
-    isMangoSeller: false,
-    friends: ["X", "Z"],
-  });
-
-  graphNoManguSeller.set("me", {
-    isMangoSeller: false,
-    friends: ["B", "D"],
-  });
-
-  graphNoManguSeller.set("D", {
-    isMangoSeller: false,
-    friends: ["W", "E", "U"],
-  });
-
-  graphNoManguSeller.set("E", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  graphNoManguSeller.set("U", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  graphNoManguSeller.set("W", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  graphNoManguSeller.set("X", {
-    isMangoSeller: false,
-    friends: ["Y"],
-  });
-
-  graphNoManguSeller.set("Y", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  graphNoManguSeller.set("Z", {
-    isMangoSeller: false,
-    friends: [],
-  });
-
-  test("existing mangoSeller in hashtable", () => {
-    expect(breadthFirstSearch("me", graph)).toEqual({
-      isMangoSeller: true,
-      friends: [],
+    expect(dijkstrasAlgorithm(graph, "Book", "Piano")).toEqual({
+      cost: 35,
+      path: ["Book", "LP", "Drums", "Piano"],
     });
   });
 
-  test("no mangoSellers in hashtable", () => {
-    expect(breadthFirstSearch("me", graphNoManguSeller)).toBe(undefined);
+  test("small graph of 4 nodes", () => {
+    const graph = new Map();
+    const start = new Map();
+    const A = new Map();
+    const B = new Map();
+    const fin = new Map();
+
+    A.set("fin", 1);
+    B.set("A", 3);
+    B.set("fin", 5);
+    start.set("A", 6);
+    start.set("B", 2);
+
+    graph.set("start", start);
+    graph.set("A", A);
+    graph.set("B", B);
+    graph.set("fin", null);
+
+    expect(dijkstrasAlgorithm(graph, "start", "fin")).toEqual({
+      cost: 6,
+      path: ["start", "B", "A", "fin"],
+    });
+  });
+
+  test("Graph A", () => {
+    const finish = new Map();
+    const D = new Map();
+    const C = new Map();
+    const B = new Map();
+    const A = new Map();
+    const start = new Map();
+
+    const graph = new Map();
+
+    D.set("finish", 1);
+    C.set("finish", 3);
+    C.set("D", 6);
+    B.set("A", 8);
+    B.set("D", 7);
+    A.set("B", 8);
+    start.set("A", 5);
+    start.set("B", 2);
+
+    graph.set("start", start);
+    graph.set("A", A);
+    graph.set("B", B);
+    graph.set("C", C);
+    graph.set("D", D);
+    graph.set("finish", finish);
+    graph.set("finish", null);
+
+    expect(dijkstrasAlgorithm(graph, "start", "finish")).toEqual({
+      cost: 10,
+      path: ["start", "B", "D", "finish"],
+    });
   });
 });
