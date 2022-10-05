af711005f7618b3f14ec98b5f57b186aceabe472
Rey Arqueza <reyarqueza@gmail.com>
2022-10-05 14:26:22 -0700

    shared state with useReducer, useContext, and createContext

--- React/gizmo-reducer-hook/src/index.js
+++ React/gizmo-reducer-hook/src/index.js
@@ -1,12 +1,51 @@
-import React, { useState } from "react";
+import React, { useReducer, useContext, createContext } from "react";
 import ReactDOM from "react-dom";
+
+const initialState = { x: 0, y: 0 };
+const SharedStateContext = createContext();
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_X": {
+      return {
+        ...state,
+        x: action.x,
+      };
+    }
+
+    case "SET_Y": {
+      return {
+        ...state,
+        y: action.y,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
+function Display() {
+  const { state } = useContext(SharedStateContext);
+  const { x, y } = state;
+
+  return (
+    <h3>
+      {x} + {y} = {Number(x) + Number(y)}
+    </h3>
+  );
+}
 
 function Slider(props) {
   const { label, id, legend } = props;
-  const [sliderVal, setSliderVal] = useState(0);
+  const { state, dispatch } = useContext(SharedStateContext);
 
   function handleChange(e) {
-    setSliderVal(e.target.value);
+    dispatch({
+      type: `SET_${label}`,
+      [id]: e.target.value,
+    });
   }
 
   return (
@@ -17,7 +56,7 @@ function Slider(props) {
           display: "flex",
         }}
       >
-        <label for="x">{label}</label>
+        <label htmlFor="x">{label}</label>
         <input
           style={{
             width: "100%",
@@ -27,25 +66,33 @@ function Slider(props) {
           min={0}
           max={50}
           onChange={handleChange}
-          value={sliderVal}
+          value={state[id]}
         />
       </div>
       <strong style={{ display: "block", textAlign: "center" }}>
-        {sliderVal}
+        {state[id]}
       </strong>
     </fieldset>
   );
 }
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const sharedState = {
+    state,
+    dispatch,
+  };
+
   return (
-    <>
-      <h1>React Hook useReducer</h1>
+    <SharedStateContext.Provider value={sharedState}>
+      <h1>React Hook useReducer, useContext, createContext</h1>
       <h2>Redux without Redux!</h2>
+      <Display />
+      <br />
       <Slider id="x" label="X" legend="First Operand" />
       <br />
       <Slider id="y" label="Y" legend="Second Operand" />
-    </>
+    </SharedStateContext.Provider>
   );
 }
 
